63befcec6258eb2d4513604047e8af833ddcefad
ussyalfaks <usmanalfaki@gmail.com>
2024-02-12 12:47:42 +0100

    coffee

---
+++ coffee/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from '../assets/Logo.png'
+
+function Header() {
+  return (
+    <>
+    <div className="flex items-center  bg-[url('../assets/p3.png')] bg-center">
+        <div>
+        <img src={Logo} alt="" />
+        </div>   
+
+        <nav>
+            <ul className='flex'>
+                <li className='mx-5'>Home</li>
+                <li className='mx-5'>AboutUs</li>
+                <li className='mx-5'>Features</li>
+                <li className='mx-5'>Purchase</li>
+                <li className='mx-5'>ContactUs</li>
+            </ul>
+        </nav>     
+    </div>
+    </>
+  )
+}
+
+export default Header
